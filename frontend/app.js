8ae08a276ed73c80327be3753939562109a4a1e7
Ken Lenard De Ocampo <kendeocampo15@gmail.com>
2026-09-15 15:51:24 +0800

    Update

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,12 @@
 const API_URL = "";
-
+const API_Key = "student-api-key-123"
 
 // GET ALL COMPOUNDS
 async function loadCompounds() {
     try {
-        const response = await fetch(`${API_URL}/compounds`);
+        const response = await fetch(`${API_URL}/api/v1/compounds`, {
+            headers: { "x-api-key": API_KEY }
+        });
         const data = await response.json();
         displayCompounds(data.compounds);
     }
